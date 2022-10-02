917e02f91bbb342d2ebfe6996029a1997fbbf68a
Nikolas Rieble <nikolas.rieble@tulip.co>
2022-10-02 22:58:58 +0200

    Use Titanic dataset

--- src/stories/advancedUsage/Marimeko.stories.jsx
+++ src/stories/advancedUsage/Marimeko.stories.jsx
@@ -21,28 +21,50 @@ export default {
   },
 };
 
-const Template = ({ data, xAxisKey }) => {
+const Template = ({ data, xAxisKey, dataKey, stackId }) => {
   return (
     <div style={{ width: 1000, height: 400 }}>
       <ResponsiveContainer>
-        <BarChart data={data}>
+        <BarChart barGap={1} barCategoryGap={1} data={data}>
           <XAxis dataKey={xAxisKey} />
           <YAxis />
           <Tooltip />
           <Legend />
-          <Bar dataKey={xAxisKey} />
+          <Bar dataKey={xAxisKey} stackId={stackId} fill="#8884d8" />
         </BarChart>
       </ResponsiveContainer>
     </div>
   );
 };
 
+// Gender | Survived | 1st Class|	2nd Class| 	3rd Class|	Crew
+// Male  	| No	     | 118      |	154      | 	422      |	670
+//        | Yes	     | 62       |	25       | 	88       |	192
+// Female	| No	     | 4        |	13       | 	106      |	3
+//        | Yes      | 141      |	93       | 	90       |	20
+const data = [
+  { gender: "male", class: 1, survived: true, count: 62 },
+  { gender: "male", class: 1, survived: false, count: 118 },
+  { gender: "male", class: 2, survived: true, count: 25 },
+  { gender: "male", class: 2, survived: false, count: 154 },
+  { gender: "male", class: 3, survived: true, count: 88 },
+  { gender: "male", class: 3, survived: false, count: 422 },
+  { gender: "male", class: 4, survived: true, count: 192 },
+  { gender: "male", class: 4, survived: false, count: 670 },
+  { gender: "female", class: 1, survived: true, count: 141 },
+  { gender: "female", class: 1, survived: false, count: 4 },
+  { gender: "female", class: 2, survived: true, count: 93 },
+  { gender: "female", class: 2, survived: false, count: 13 },
+  { gender: "female", class: 3, survived: true, count: 90 },
+  { gender: "female", class: 3, survived: false, count: 106 },
+  { gender: "female", class: 4, survived: true, count: 20 },
+  { gender: "female", class: 4, survived: false, count: 3 },
+];
+
 export const SimpleStory = Template.bind({});
 SimpleStory.args = {
-  data: [
-    { a: 1, b: 2 },
-    { a: 1, b: 2 },
-    { a: 3, b: 2 },
-  ],
-  xAxisKey: "a",
+  data,
+  xAxisKey: "class",
+  dataKey: "count",
+  stackId: "gender",
 };
